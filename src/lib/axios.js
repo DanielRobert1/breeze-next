d363fbaad9e56bdb991d672f09653bdf43080cc6
Daniel Aigbe <daniel4life1@gmail.com>
2023-02-05 15:06:52 +0100

    .

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,10 +3,10 @@ import Axios from 'axios'
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
-        'X-Requested-With': 'XMLHttpRequest',
         'Content-Type': 'application/json',
         Accept: 'application/json',
     },
+    withCredentials: true,
 })
 
 export default axios
